1e03789dcc792563a3f019bd9b24f044a052ec92
Jon Drobny <drobny2@illinois.edu>
2020-09-08 13:41:07 -0500

    Added Newton-polished version of find_roots.

--- src/lib.rs
+++ src/lib.rs
@@ -131,7 +131,6 @@ pub mod chebyshev {
             }
             x = x1;
         }
-        println!("Failed to converge.");
         return x0
     }
 
@@ -203,6 +202,22 @@ pub mod chebyshev {
             }
         }
         roots
+    }
+
+    pub fn find_roots_with_newton_polishing(f: &dyn Fn(f64) -> f64, df: &dyn Fn(f64) -> f64, a: f64, b: f64, N0: usize, epsilon: f64, N_max: usize, complex_threshold: f64, truncation_threshold: f64, interval_limit: f64) -> Vec<f64> {
+        let roots = find_roots(f, a, b, N0, epsilon, N_max, complex_threshold, truncation_threshold, interval_limit);
+        let mut polished_roots: Vec<f64> = Vec::new();
+
+        for &root in roots.iter() {
+
+            let root_refined = newton_polish(&f, &df, root, 100, epsilon);
+            let correction = newton_correction(&f, &df, root_refined);
+
+            if (correction) < 1E-3 {
+                polished_roots.push(root);
+            }
+        }
+        polished_roots
     }
 
     fn chebyshev_coefficients(f: &dyn Fn(f64) -> f64, a: f64, b: f64, N: usize) -> Array1<f64> {
